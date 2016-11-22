a35bac1c3e65164fa2ee63c78c71675067cdc576
bstearns <blaine.stearns@gmail.com>
2016-11-22 13:28:21 -0700

    Add mobile service inital commit

---
+++ Projects/PCLConstruct.Api/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(PCLConstruct.Api.Startup))]
+
+namespace PCLConstruct.Api
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
