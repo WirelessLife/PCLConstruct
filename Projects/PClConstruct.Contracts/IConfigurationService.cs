88a69b317d77ce996bcf74b9e0b22c7f864a69da
bstearns <blaine.stearns@gmail.com>
2016-11-22 13:01:44 -0700

    Initial Commit of the the Client

---
+++ Projects/PClConstruct.Contracts/IConfigurationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PClConstruct.Contracts
+{
+    public interface IConfigurationService
+    {
+        string GetStorageConnection(string storageName);
+
+        string GetStorageName(string context);
+    }
+}
